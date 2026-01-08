f2ab8bd0ffac8698570be94d6ea9c183a712277d
Ranjiit Sir - FORK INFOSYSTEMS <ranjiitsir@gmail.com>
2026-01-09 00:05:26 +0530

    Add files via upload

---
+++ 00 Theory/Nested if.. Else.C
@@ -0,0 +1,29 @@
+
+if(<condition1>)
+{
+            if(<condition2>)
+            {
+                    // True Statements Block of Condition 1 & 2
+            }
+            else
+            {
+                    // True Statements Block of Condition 1
+            }
+}
+else if(<condition3>)
+{
+            if(<condition4>)
+            {
+                    // True Statements Block of Condition 3 & 4
+            }
+            else
+            {
+                    // True Statements Block of Condition 3
+            }
+}
+else
+{
+            // False Block For Conditions 1 & 3
+}
+
+

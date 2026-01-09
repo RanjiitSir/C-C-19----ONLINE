5c80d708bf591111f0bd70c338fdde1fa5c7567f
Ranjiit Sir - FORK INFOSYSTEMS <ranjiitsir@gmail.com>
2026-01-09 20:12:58 +0530

    Add files via upload

---
+++ Struct Online 2026/01 Struct Home Work.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct Stud
+{
+    int RNo;
+    char Name[40];
+    int Phy;
+    int Chem;
+    int Maths;
+    int Tot;
+    float Per;
+};
+
+int main( )
+{
+    struct Stud S1;
+
+    ///
+
+    getch();
+    return 0;
+}
---
+++ Struct Online 2026/05 Basics of Structures - Accept Multiple Student Details and Display Them using Array and Loop.C
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct Stud
+{
+    int RNo;
+    char Name[40];
+    long long int MobNo;
+    float Per;
+};
+
+int main( )
+{
+    int i = 0;
+    struct Stud Std[3] = {};
+
+    for(i = 0; i < 3; i++)          /// Accept All 3 Student Details With Loop
+    {
+        printf("\n Enter Student No %d Details => \n", i+1);
+
+        printf("\n Enter Students Roll Number : ");
+        scanf("%d",&Std[i].RNo);
+
+        fflush(stdin);
+
+        printf("\n Enter Students Name : ");
+        gets(Std[i].Name);
+
+        printf("\n Enter Students Mobile Number : ");
+        scanf("%lld",&Std[i].MobNo);
+
+        printf("\n Enter Students Percentage : ");
+        scanf("%f",&Std[i].Per);
+    }
+
+    printf("\n Enter Any Key To See All Details : ");
+    getch();
+
+    printf("\n\n Entered 3 Students Details Are => \n\n");
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("\n Roll No    : %d.", Std[i].RNo);
+        printf("\n Name       : %s.", Std[i].Name);
+        printf("\n Mobile No  : %lld.", Std[i].MobNo);
+        printf("\n Percentage : %f.", Std[i].Per);
+        printf("\n\n ============== *** ==============\n");
+    }
+
+    getch();
+    return 0;
+}
